f9bd10555901703a90ba8a6baf09091d7eb8dc41
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2023-04-20 09:38:38 +0100

    restart - redesigned

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import Navbar from './components/navbar/Navbar';
-import Homepage from './pages/homepage/Homepage';
 import './App.scss';
 
 function App() {
   return (
     <div className='app__container'>
-      <Navbar />
-      <div className='app__pages'>
-        <Homepage />         
-      </div>
     </div>
   );
 }
